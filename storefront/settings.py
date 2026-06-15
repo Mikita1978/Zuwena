223de8353dc8924865affbbc340671e132ec82b9
michaomondi <mikeomondi2006@gmail.com>
2026-06-15 11:18:50 +0300

    Fix Render build requirements path

--- storefront/settings.py
+++ storefront/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1cx8u!j2ran0a+voixs-_wsxl3v#@-k@mlw95orprd!m08rl6x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
